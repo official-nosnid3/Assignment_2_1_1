aae99aba247bb261724f02967cbfb8f979e545bb
Edinson Cabral <Edinson.Cabral@mssa.newhorizons.com>
2024-11-11 13:16:09 -0600

    Added assignment 2.1.2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace Assignment_2_1_1
              * Write a class: “Student” with private data members: StudentId ( int), StudentFname (string), 
              * StudentLname (string ), StudentGrade ( char ) and public properties for each data member. 
              * Instantiate the class and assign data to properties. Display the data back on console.
-             * */
+             * 
+             * Assignment 2.1.2
+             * Design a class hierarchy of your choice. (Need only class files )
+             */
 
             Student student = new Student();
 
@@ -29,10 +32,12 @@ namespace Assignment_2_1_1
             student.Grade = Convert.ToChar( Console.Read() );
 
             Console.WriteLine($"\nYou have created a new profile for {student.FirstName} {student.LastName} with ID number {student.IdNumber} and submitted a grade ({student.Grade})");
+            
+            Console.WriteLine($"\n{student.FirstName} {student.LastName}'s teacher is {student.TeacherFullname} in classroom {student.RoomNumber}");
         }
     }
 
-    class Student
+    class Student : Classroom
     {
         private int _idNumber;
         public int IdNumber
@@ -60,6 +65,21 @@ namespace Assignment_2_1_1
         {
             get { return _grade; }
             set { _grade = char.ToUpper(value); }
+        }
+    }
+
+    class Classroom
+    {
+        private string _teacherFullName = "John Devries";
+        public string TeacherFullname
+        {
+            get { return _teacherFullName; }
+        }
+
+        private string _roomNumber = "16";
+        public string RoomNumber
+        {
+            get { return _roomNumber; }
         }
     }
 }
